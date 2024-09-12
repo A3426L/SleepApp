77eae3fa8aa276e7d2d173df0ceb17c8e09a68b9
A3426L <mikan7738@gmail.com>
2024-09-12 23:22:02 +0900

    edit migrate

--- docker/server/app.py
+++ docker/server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy import text 
 
 app = Flask(__name__)
 
@@ -13,6 +14,18 @@ migrate = Migrate(app, db)
 @app.route('/')
 def hello():
 	return "Hello World!"
+
+@app.route('/check-db')
+def check_db():
+    try:
+        # テキスト形式でSQLを実行
+        result = db.session.execute(text('SELECT 1')).scalar()
+        if result == 1:
+            return "Database connection successful!", 200
+        else:
+            return "Database connection failed!", 500
+    except Exception as e:
+        return str(e), 500
 
 if __name__ == "__main__":
 	app.run(debug=True)
