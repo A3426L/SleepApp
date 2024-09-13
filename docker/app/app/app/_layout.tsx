738d2ec3cf70a7e01c6584e97844d82959b463e6
motoki <kerokerokey0703@gmail.com>
2024-09-13 23:23:53 +0900

    edit

--- docker/app/app/app/_layout.tsx
+++ docker/app/app/app/_layout.tsx
@@ -3,7 +3,14 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="(pages)/home" options={{
+        title: "Home",
+        headerBackVisible: false,
+        gestureEnabled: false,
+        headerStyle:{
+          backgroundColor: "red",
+        }
+      }}/>
     </Stack>
   );
 }
