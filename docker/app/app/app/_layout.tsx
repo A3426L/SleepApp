6c42569095c14b1a260e9ae1a353774d11155992
motoki <kerokerokey0703@gmail.com>
2024-09-14 18:06:46 +0900

    add_tab

--- docker/app/app/app/_layout.tsx
+++ docker/app/app/app/_layout.tsx
@@ -1,3 +1,30 @@
+import { Stack, Tabs} from "expo-router";
+import {useRouter} from 'expo-router';
+import { Modal } from "react-native";
+
+export default function RootLayout() {
+  const router = useRouter();
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="(pages)"
+        options={{
+          title: 'Home',
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="test_tabs"
+        options={{
+          title: 'test_tabs' ,
+          headerShown: false,
+        }}
+      />
+    </Tabs>
+  );
+}
+
+/*
 import { Stack} from "expo-router";
 import {useRouter} from 'expo-router';
 import { Modal } from "react-native";
@@ -25,6 +52,14 @@ export default function RootLayout() {
         //headerBackVisible: false,
         //gestureEnabled: false,
       }}/>
+      <Stack.Screen name="(pages)/test_modal" options={{
+        title: "demo_chat",
+        presentation: "modal",
+        animation:"slide_from_left"
+        //headerBackVisible: false,
+        //gestureEnabled: false,
+      }}/>
     </Stack>
   );
 }
+*/
