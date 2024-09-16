4035cbf40a8ca340c4ccbb1e1cf2cb4a5ace593e
松田 <aoisora9000@gmail.com>
2024-09-16 21:26:50 +0900

    new

--- docker/server/app.py
+++ docker/server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask , jsonify
+from flask import Flask , jsonify , request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy import text 
@@ -21,6 +21,12 @@ class User2(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
 
+    #メッセージモデル
+class Message(db.Model):
+     id = db.Column(db.Integer,primary_key=True)
+     user_id = db.Column(db.String(15),nullable=False)  
+     message = db.Column(db.String(100),nullable=False)
+
 @app.route('/')
 def hello():
      return "Hello World!"
@@ -31,7 +37,7 @@ def create_db():
     with app.app_context():
         db.create_all()
         return "Database tables created."
-
+#お試し用
 @app.route('/add-user-test', methods=['GET'])
 def add_user_test():
     id = "2"
@@ -41,7 +47,17 @@ def add_user_test():
         db.session.add(new_user)
         db.session.commit()
     return "Add data succusess."
-
+#チャット用
+@app.route('/add-chat-test', methods=['GET'])
+def add_chat_test():
+    id = "1"
+    user_id = "abc123"
+    message = "おはよう"
+    new_chat = Message(id=id, user_id=user_id,message=message)
+    with app.app_context():
+        db.session.add(new_chat)
+        db.session.commit()
+    return "Add chatdata succusess."
 
 
 @app.route('/check-db')
@@ -58,6 +74,29 @@ def check_db():
 @app.route('/api/data', methods=['GET'])
 def get_data():
     return jsonify({"message": "Hello from Flask!", "user_id": 1})
+
+
+#チャット用
+@app.route('/chat/<int:user_id>',methods=['POST','GET'])
+def chat(user_id):
+     #送信データからルーム名を取得
+     current_user_id = request.form.get('user_id')
+     current_messages = Message.query.filter_by(user_id=user_id).all()
+
+     #chat_info = [{'user_id': msg.user_id, 'messages': msg.message} for msg in current_messages]
+     return jsonify({'messages':current_messages, 'user_id':current_user_id})
+
+#ユーザーがメッセージを送信した時の処理
+@app.route('/send_message',methods=['POST'])
+def send_message():
+          message_content = request.form.get('messages')
+          user_id_content = request.form.get('user_id')
+          #メッセージを保存
+          message = Message(user_id=user_id_content,message=message_content)
+          db.session.add(message)
+          db.session.commit()
+
+
 
 if __name__ == "__main__":
      
