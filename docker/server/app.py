a591fed96d503fb1ca6b6b31612fdc360c819c20
A3426L <mikan7738@gmail.com>
2024-09-16 18:49:56 +0900

    edit_db

--- docker/server/app.py
+++ docker/server/app.py
@@ -13,9 +13,36 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+class User2(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
 @app.route('/')
 def hello():
      return "Hello World!"
+
+@app.route('/createdb')
+def create_db():
+    """Create the database tables."""
+    with app.app_context():
+        db.create_all()
+        return "Database tables created."
+
+@app.route('/add-user-test', methods=['GET'])
+def add_user_test():
+    id = "2"
+    name = "ううう"
+    new_user = User(id=id, name=name)
+    with app.app_context():
+        db.session.add(new_user)
+        db.session.commit()
+    return "Add data succusess."
+
+
 
 @app.route('/check-db')
 def check_db():
@@ -33,29 +60,7 @@ def get_data():
     return jsonify({"message": "Hello from Flask!", "user_id": 1})
 
 if __name__ == "__main__":
-     app.run(debug=True)
      
-#      from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-
-# app = Flask(__name__)
-
-# # データベース URI を直接設定
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mysql:mysql@db:3306/mysql_test'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# db = SQLAlchemy(app)
-
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(50), nullable=False)
-
-# def create_db():
-#     """Create the database tables."""
-#     with app.app_context():
-#         db.create_all()
-#         print("Database tables created.")
-
-# if __name__ == '__main__':
-#     create_db()
-#     app.run(debug=True)
+     app.run(debug=True)
+     #create_db()
+     
