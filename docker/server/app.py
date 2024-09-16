13a20af93c625f2e546fa7103031cdc6c7a33cce
armada15881214 <go2321312@gmail.com>
2024-09-17 00:06:15 +0900

    edit

--- docker/server/app.py
+++ docker/server/app.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app) 
-
+#test
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mysql:mysql@db:3306/mysql_test'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
