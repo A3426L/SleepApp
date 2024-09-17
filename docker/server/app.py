2d6ed4e9f749bccc943829fbf777a4002ed0473e
A3426L <mikan7738@gmail.com>
2024-09-18 05:20:35 +0900

    add post_func

--- docker/server/app.py
+++ docker/server/app.py
@@ -1,8 +1,10 @@
-from flask import Flask , jsonify
+from flask import Flask , jsonify , request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from sqlalchemy import text 
+from sqlalchemy import text , or_
 from flask_cors import CORS
+
+from datetime import datetime as dt
 
 
 app = Flask(__name__)
@@ -42,6 +44,83 @@ def add_user_test():
         db.session.commit()
     return "Add data succusess."
 
+
+
+
+def format_datetime_to_string(dt_obj):
+    return dt_obj.strftime('%Y%m%d%H%M%S%f')
+
+
+@app.route('/date')
+def get_current_datetime():
+    datetime = dt.now()
+    strdatatime = format_datetime_to_string(datetime)
+    return strdatatime
+
+
+class Old(db.Model):
+    __tablename__ = 'old'  # テーブル名
+
+    room_name = db.Column(db.String(50), primary_key=True)
+    user_id0 = db.Column(db.String(50), nullable=True)
+    user_id1 = db.Column(db.String(50), nullable=True)
+    user_id2 = db.Column(db.String(50), nullable=True)
+    user_id3 = db.Column(db.String(50), nullable=True)
+    user_id4 = db.Column(db.String(50), nullable=True)
+    end_time = db.Column(db.DateTime, nullable=True)
+
+class Post(db.Model):
+    __tablename__ = 'post'
+
+    id = db.Column(db.Integer, primary_key=True)
+    room_name = db.Column(db.String(50))
+    user_id = db.Column(db.String(50), nullable=False)
+    theme = db.Column(db.String(50), nullable=True)
+    post_txt = db.Column(db.Text, nullable=True)
+
+@app.route('/postView_group')
+def postView_group():
+    #data = request.get_json()
+    user_id = "a3426l"#data['user_id']
+
+    old_results = Old.query.filter(
+        or_(
+            Old.user_id0 == user_id,
+            Old.user_id1 == user_id,
+            Old.user_id2 == user_id,
+            Old.user_id3 == user_id,
+            Old.user_id4 == user_id
+        )
+    ).order_by(Old.end_time.desc()).all()
+
+    if old_results:
+        latest_old = old_results[0]
+        latest_room_name = latest_old.room_name,
+        # old_list = {
+        #         "room_name": latest_old.room_name,
+        #         "user_id0": latest_old.user_id0,
+        #         "user_id1": latest_old.user_id1,
+        #         "user_id2": latest_old.user_id2,
+        #         "user_id3": latest_old.user_id3,
+        #         "user_id4": latest_old.user_id4,
+        #         "end_time": latest_old.end_time
+        #     } 
+    else:
+        old_list = {}
+
+    posts = Post.query.filter_by(room_name=latest_room_name).all()
+
+    post_list = [
+        {
+            "id": post.id,
+            "user_name": post.user_id,
+            "theme": post.theme,
+            "post_txt": post.post_txt
+        }
+        for post in posts
+    ]
+
+    return jsonify(post_list)
 
 
 @app.route('/check-db')
