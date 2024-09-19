7d8c149317528656936bcc60b2cd13f0c0f7f55b
NaokiNishihira <dadakko1206@gmail.com>
2024-09-19 15:14:24 +0900

    fix:ChangeDate

--- docker/app/app/components/ChangeDate.tsx
+++ docker/app/app/components/ChangeDate.tsx
@@ -1,10 +1,11 @@
 export default function ChangeDate(Date_S:string) {
-    const year = Number(Date_S.slice(0, 4));
-    const month = Number(Date_S.slice(4, 6));
-    const day = Number(Date_S.slice(6, 8));
-    const hours = Number(Date_S.slice(8, 10));
-    const minutes = Number(Date_S.slice(10, 12));
-    const seconds = Number(Date_S.slice(12));
-    const dateFromYMDHMS = new Date(year,(month-1),day,hours,minutes,seconds);
+    const timeZoneOffset = '+09:00';
+    const year = (Date_S.slice(0, 4));
+    const month = (Date_S.slice(4, 6));
+    const day = (Date_S.slice(6, 8));
+    const hours = (Date_S.slice(8, 10));
+    const minutes = (Date_S.slice(10, 12));
+    const seconds = (Date_S.slice(12));
+    const dateFromYMDHMS = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}${timeZoneOffset}`;
     return dateFromYMDHMS;
 }
