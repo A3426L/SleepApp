737edf753c4019a2f2551a4859378708d3997163
NaokiNishihira <dadakko1206@gmail.com>
2024-09-19 15:14:12 +0900

    fix:chat.tsx

--- docker/app/app/app/chat.tsx
+++ docker/app/app/app/chat.tsx
@@ -5,6 +5,7 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Link, usePathname, useRouter, useFocusEffect } from 'expo-router';
 import axios from 'axios';
 import { useGlobalContext } from './GlobalContext';
+import ChangeDate from '../components/ChangeDate';
 
 const home_image = require("@/assets/images/chat_image.png");
 
@@ -225,11 +226,13 @@ export const App: React.FC = () => {
 
     const fetchDataAndStartProgress = async () => {
       try {
-        const response = await axios.get('http://172.20.10.8/chat_start');
+        const response = await axios.post('http://172.20.10.8/chat_start',{
+          user_id: userIdglobal,
+        });
         const now = new Date().getTime();
-        const start = new Date(response.data.startTime).getTime();
-        const end = new Date(response.data.endTime).getTime();
-
+        const start = new Date(ChangeDate(response.data.startTime)).getTime();
+        const end = new Date(ChangeDate(response.data.endTime)).getTime();
+        console.log("aaaaaaaaaaa",end);
         //リーダーかの判断
         if (response.data.user_id0 === "1") {
           setIsEditable(true); // リーダーの場合
@@ -244,8 +247,8 @@ export const App: React.FC = () => {
         const totalDuration = end - start;
         const elapsedTime = current - start;
 
-        console.log('サーバーからの開始時刻（JST）:', new Date(response.data.startTime).toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' }));
-        console.log('サーバーからの終了時刻（JST）:', new Date(response.data.endTime).toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' }));
+        console.log('サーバーからの開始時刻（JST）:', start);
+        console.log('サーバーからの終了時刻（JST）:', end);
         console.log('クライアントの現在時刻:', new Date().toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' }));
 
         // プログレスバーを開始
@@ -276,13 +279,15 @@ export const App: React.FC = () => {
 
 
   const test = () => {
-    // axios.get('http://172.20.10.8/api/data')
-    //   .then(test_data => {
-    //     console.log(test_data.data);
+    // axios.post('http://172.20.10.8/randam_theme',{
+    //   user_id: userIdglobal,
+    // })
+    //   .then(random_theme => {
+    //     console.log(random_theme.data.theme);
     //   })
     //   .catch(error => console.error("error", error));
-    const newUserId = '2'; // ここに設定したい値を設定
-    setUserIdglobal(newUserId);
+    // const newUserId = '2'; // ここに設定したい値を設定
+    // setUserIdglobal(newUserId);
   };
 
   return (
@@ -475,3 +480,325 @@ const styles = StyleSheet.create({
     transform: [{ rotate: '20deg' }], // アイコンを90度回転
   },
 });
+
+
+
+// from flask import Flask , jsonify, request
+// from flask_sqlalchemy import SQLAlchemy
+// from flask_migrate import Migrate
+// from sqlalchemy import text 
+// from flask_cors import CORS
+// from datetime import datetime, timedelta
+
+
+// app = Flask(__name__)
+// CORS(app)
+
+// app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mysql:mysql@db:3306/mysql_test'
+// app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+// db = SQLAlchemy(app)
+
+// @app.route('/')
+// def hello():
+//      return "Hello World!"
+
+// @app.route('/check-db')
+// def check_db():
+//     try:
+//         result = db.session.execute(text('SELECT 1')).scalar()
+//         if result == 1:
+//             return "Database connection successful!", 200
+//         else:
+//             return "Database connection failed!", 500
+//     except Exception as e:
+//         return str(e), 500
+    
+// @app.route('/testpost', methods=['POST'])
+// def testpost():
+//     data = request.get_json()
+//     if 'user_id' in data:
+//         return jsonify({"postView_group":[
+//     {
+//         "id" : "1",
+//         "user_name": "sample",
+//         "theme":   "sample",
+//         "post_txt": "sample"
+//     },
+//     {
+//         "id" : "2",
+//         "user_name": "sample2",
+//         "theme":   "sample2",
+//         "post_txt": "sample2"
+//     }
+// ]}) 
+//     else:
+//         return jsonify({"error": "user_id not provided"}), 400
+    
+// @app.route('/api/data', methods=['GET'])
+// def get_data():
+//     return jsonify({
+//         "messages": [
+//           {
+//                "text": "Hello from Flask!",
+//                "msg_id": "1",
+//                "user_id": "3"
+//           },
+//           {
+//                "text": "Another message",
+//                "msg_id": "2",
+//                "user_id": "1"
+//           },
+//           {
+//                "text": "Yet another message",
+//                "msg_id": "3",
+//                "user_id": "2"
+//           },
+//           {
+//                "text": "Final message",
+//                "msg_id": "4",
+//                "user_id": "4"
+//           },
+//         ],
+//     })
+
+// @app.route('/api/data/post', methods=['POST'])
+// def post_data():
+//      # POSTリクエストのボディからJSONデータを取得
+//     data = request.json
+//     # 取得したデータをそのまま返す
+//     return jsonify(data)
+
+// @app.route('/chat_start', methods=['POST'])
+// def get_endtime():
+//     # 固定された開始時刻と終了時刻を設定（JSTのISOフォーマット）
+//     fixed_start_time = "20240919140000"  # 固定された開始時刻
+//     fixed_end_time = "20240919163000"    # 固定された終了時刻
+    
+//     # 開始時刻と終了時刻をJSONで返す
+//     return jsonify({'user_id0': "1",'startTime': fixed_start_time, 'endTime': fixed_end_time})
+
+// # リーダーかどうかを判断
+// @app.route('/api/leader', methods=['POST'])
+// def post_leader():
+//     # POSTリクエストからJSONデータを取得
+//     data = request.json
+
+//     # 'value'キーがリクエストボディに存在するか確認
+//     if 'value' in data:
+//         # 'value'の値が1の場合は1を返す
+//         if data['value'] == 1:
+//             return jsonify({'result': 1})
+//         else:
+//             return jsonify({'result': 0})
+//     else:
+//         # 'value'キーが存在しない場合はエラーメッセージを返す
+//         return jsonify({'error': 'Missing "value" in request'}), 400
+
+// #タイトル受け取る
+// @app.route('/api/title', methods=['POST'])
+// def post_and_return():
+//     # POSTリクエストからJSONデータを取得
+//     data = request.json
+
+//     # 受け取ったデータをそのまま返す
+//     return jsonify(data)
+
+// #タイトル受け取る
+// @app.route('/api/title_test', methods=['POST'])
+// def post_title_test():
+//     # POSTリクエストからJSONデータを取得
+//     data = request.json
+
+//     # 受け取ったデータをそのまま返す
+//     return jsonify({'title': "画面遷移だよん!"})
+
+// #タイトル受け取る
+// @app.route('/api/test_post', methods=['POST'])
+// def teat_post():
+//     # POSTリクエストからJSONデータを取得
+//     data = request.json
+
+//     # 受け取ったデータをそのまま返す
+//     return jsonify(data)
+
+// @app.route('/matching', methods=['POST'])
+// def matching():
+//      # POSTリクエストのボディからJSONデータを取得
+//     data = request.json
+//     # 取得したデータをそのまま返す
+//     return jsonify({'flag': "true"})
+
+// @app.route('/matching_start', methods=['POST'])
+// def matching_start():
+//      # POSTリクエストのボディからJSONデータを取得
+//     data = request.json
+//     # 取得したデータをそのまま返す
+//     return jsonify({'flag': "true"})
+
+// @app.route('/signup', methods=['POST'])
+// def signup():
+//     data = request.get_json()
+//     if 1:
+//         return jsonify(
+//     {
+//        "flag":"true"
+//     },
+
+// ) 
+//     else:
+//         return jsonify({"error": "user_id not provided"}), 400
+    
+    
+    
+// @app.route('/login', methods=['POST'])
+// def login():
+//     data = request.get_json()
+//     if 1:
+//         return jsonify(
+//     {
+//        "flag":"true"
+//     },
+
+// ) 
+//     else:
+//         return jsonify({"error": "user_id not provided"}), 400
+    
+// @app.route('/get_userName', methods=['POST'])
+// def get_userName():
+//     data = request.get_json()
+//     if 1:
+//         return jsonify(
+//     {
+//        "user_name":"sample"
+//     },
+
+// ) 
+//     else:
+//         return jsonify({"error": "user_id not provided"}), 400
+// @app.route('/postView_group', methods=['POST'])
+// def postView_group():
+//     data = request.get_json()
+//     if 1:
+//         return jsonify(
+//     [{
+//         "id":"8",
+//         "user_name":"name8",
+//         "theme":"theme8",
+//         "post_txt":"疲れた",
+//     },
+// ]
+
+// ) 
+//     else:
+//         return jsonify({"error": "user_id not provided"}), 400
+    
+    
+// @app.route('/postView_all', methods=['GET'])
+// def postView_all():
+//     return jsonify(
+//  [
+//     {
+//       "id":"1",
+//       "user_name":"name1",
+//       "theme":"theme1",
+//       "post_txt":"眠い",
+//     },
+//     {
+//       "id":"2",
+//       "user_name":"name2",
+//       "theme":"theme2",
+//       "post_txt":"眠い",
+//     },
+//     {
+//       "id":"3",
+//       "user_name":"name3",
+//       "theme":"theme3",
+//       "post_txt":"眠い",
+//     },
+//     {
+//       "id":"4",
+//       "user_name":"name4",
+//       "theme":"theme4",
+//       "post_txt":"眠い",
+//     },
+//     {
+//       "id":"5",
+//       "user_name":"name5",
+//       "theme":"theme5",
+//       "post_txt":"眠い",
+//     },
+//     {
+//       "id":"6",
+//       "user_name":"name6",
+//       "theme":"theme8",
+//       "post_txt":"眠い",
+//     },
+//     {
+//       "id":"7",
+//       "user_name":"name7",
+//       "theme":"theme7",
+//       "post_txt":"眠い",
+//     },
+//     {
+//       "id":"8",
+//       "user_name":"name8",
+//       "theme":"theme8",
+//       "post_txt":"眠い",
+//     },
+
+//   ]
+
+// ) 
+    
+// @app.route('/movePost', methods=['POST'])
+// def movePost():
+//     data = request.get_json()
+//     if 1:
+//         return jsonify(
+//     {
+//        "theme":"sample"
+//     },
+
+// ) 
+        
+// @app.route('/post', methods=['POST'])
+// def post():
+//     data = request.get_json()
+//     if 1:
+//         return jsonify(
+//     {
+//         "flag":"true" 
+//     },
+
+// ) 
+
+
+
+// if __name__ == "__main__":
+//      app.run(debug=True)
+     
+// #      from flask import Flask
+// # from flask_sqlalchemy import SQLAlchemy
+
+// # app = Flask(__name__)
+
+// # # データベース URI を直接設定
+// # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mysql:mysql@db:3306/mysql_test'
+// # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+// # db = SQLAlchemy(app)
+
+// # class User(db.Model):
+// #     id = db.Column(db.Integer, primary_key=True)
+// #     name = db.Column(db.String(50), nullable=False)
+
+// # def create_db():
+// #     """Create the database tables."""
+// #     with app.app_context():
+// #         db.create_all()
+// #         print("Database tables created.")
+
+// # if __name__ == '__main__':
+// #     create_db()
+// #     app.run(debug=True)
